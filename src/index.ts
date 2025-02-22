f14ca3808aca255773d86d792a7d7f48b4780f93
Fotis Papadogeorgopoulos <fotis.papadogeorgopoulos@wolt.com>
2025-02-22 15:25:13 +0200

    Fix publishing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-import mediaPrefersReducedMotionPlugin from "./rules/media-prefers-reduced-motion";
+import mediaPrefersReducedMotionPlugin from "./rules/media-prefers-reduced-motion.js";
+import focusStyleUseOutline from "./rules/focus-style-use-outline.js";
 
-export default [mediaPrefersReducedMotionPlugin];
+export default [mediaPrefersReducedMotionPlugin, focusStyleUseOutline];
