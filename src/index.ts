ed9c05014747432b5cb1df9da615b616a7c0aab4
Fotis Papadogeorgopoulos <fotis.papadogeorgopoulos@wolt.com>
2025-02-22 17:43:18 +0200

    Initial CI setup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import mediaPrefersReducedMotionPlugin from "./rules/media-prefers-reduced-motion.js";
 import focusStyleUseOutline from "./rules/focus-style-use-outline.js";
 
-export default [mediaPrefersReducedMotionPlugin, focusStyleUseOutline];
+export default [focusStyleUseOutline];
